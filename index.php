75c466ce733d8faab240be76783efcdc1e091eb8
Leidy Jaimes <leidyjaimes0711@gmail.com>
2024-06-21 11:56:19 -0500

    de html a php

--- index.php
+++ index.php
@@ -1,41 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienes Raices</title>
-    <link rel="stylesheet" href="./build/css/app.css">
-
-</head>
-<body>
-
-<header class="header inicio">
-    <div class="contenedor contenido-header">
-        <div class="barra">
-            <a href="/">
-                <img src="build/img/logo.svg" alt="Logotipo de Bienes Raices">
-            </a>
-
-            <div class="mobile-menu">
-                <img src="./build/img/barras.svg" alt="icono menu responsive">
-            </div>
-
-            <div class="derecha">
-                <img class="dark-mode-boton" src="./build/img/dark-mode.svg">
-                <nav class="navegacion">
-                    <a href="./nosotros.html">Nosotros</a>
-                    <a href="./anuncios.html">Anuncios</a>
-                    <a href="./blog.html">Blog</a>
-                    <a href="./contacto.html">Contacto</a>
-                </nav>
-            </div>
-
-
-        </div> <!--.barra-->
-
-        <h1>Venta de Casas y Departamentos  Exclusivos de Lujo</h1>
-    </div>
-</header>
+<?php
+    include "./includes/templates/header.php"
+?>
 
 <main class="contenedor seccion">
     <h1>Más Sobre Nosotros</h1>
@@ -52,7 +17,7 @@
             <p>Possimus, suscipit repudiandae. Autem deserunt aliquid deleniti sit minus consectetur obcaecati molestiae dolorem natus dolores reiciendis tempore, explicabo cum nobis laudantium. Voluptates?</p>
         </div>
         <div class="icono">
-            <img src="build/img/icono3.svg" alt="Icono Tiempo" loading="lazy">
+            <img src="./build/img/icono3.svg" alt="Icono Tiempo" loading="lazy">
             <h3>A Tiempo</h3>
             <p>Possimus, suscipit repudiandae. Autem deserunt aliquid deleniti sit minus consectetur obcaecati molestiae dolorem natus dolores reiciendis tempore, explicabo cum nobis laudantium. Voluptates?</p>
         </div>
@@ -85,12 +50,12 @@
                         <p>3</p>
                     </li>
                     <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
+                        <img class="icono" loading="lazy" src="./build/img/icono_dormitorio.svg" alt="icono habitaciones">
                         <p>4</p>
                     </li>
                 </ul>
 
-                <a href="anuncio.html" class="boton-amarillo-block">
+                <a href="anuncio.php" class="boton-amarillo-block">
                     Ver Propiedad
                 </a>
             </div><!--.contenido-anuncio-->
@@ -98,9 +63,9 @@
 
         <div class="anuncio">
             <picture>
-                <source srcset="build/img/anuncio2.webp" type="image/webp">
-                <source srcset="build/img/anuncio2.jpg" type="image/jpeg">
-                <img loading="lazy" src="build/img/anuncio2.jpg" alt="anuncio">
+                <source srcset="./build/img/anuncio2.webp" type="image/webp">
+                <source srcset="./build/img/anuncio2.jpg" type="image/jpeg">
+                <img loading="lazy" src="./build/img/anuncio2.jpg" alt="anuncio">
             </picture>
 
             <div class="contenido-anuncio">
@@ -110,20 +75,20 @@
 
                 <ul class="iconos-caracteristicas">
                     <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
+                        <img class="icono" loading="lazy" src="./build/img/icono_wc.svg" alt="icono wc">
                         <p>3</p>
                     </li>
                     <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                        <img class="icono" loading="lazy" src="./build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
                         <p>3</p>
                     </li>
                     <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
+                        <img class="icono" loading="lazy" src="./build/img/icono_dormitorio.svg" alt="icono habitaciones">
                         <p>4</p>
                     </li>
                 </ul>
 
-                <a href="anuncio.html" class="boton-amarillo-block">
+                <a href="anuncio.php" class="boton-amarillo-block">
                     Ver Propiedad
                 </a>
             </div><!--.contenido-anuncio-->
@@ -131,9 +96,9 @@
 
         <div class="anuncio">
             <picture>
-                <source srcset="build/img/anuncio3.webp" type="image/webp">
-                <source srcset="build/img/anuncio3.jpg" type="image/jpeg">
-                <img loading="lazy" src="build/img/anuncio3.jpg" alt="anuncio">
+                <source srcset="./build/img/anuncio3.webp" type="image/webp">
+                <source srcset="./build/img/anuncio3.jpg" type="image/jpeg">
+                <img loading="lazy" src="./build/img/anuncio3.jpg" alt="anuncio">
             </picture>
 
             <div class="contenido-anuncio">
@@ -143,20 +108,20 @@
 
                 <ul class="iconos-caracteristicas">
                     <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
+                        <img class="icono" loading="lazy" src="./build/img/icono_wc.svg" alt="icono wc">
                         <p>3</p>
                     </li>
                     <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                        <img class="icono" loading="lazy" src="./build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
                         <p>3</p>
                     </li>
                     <li>
-                        <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
+                        <img class="icono" loading="lazy" src="./build/img/icono_dormitorio.svg" alt="icono habitaciones">
                         <p>4</p>
                     </li>
                 </ul>
 
-                <a href="anuncio.html" class="boton-amarillo-block">
+                <a href="anuncio.php" class="boton-amarillo-block">
                     Ver Propiedad
                 </a>
             </div><!--.contenido-anuncio-->
@@ -165,14 +130,14 @@
     </div> <!--.contenedor-anuncios-->
 
     <div class="alinear-derecha">
-        <a href="anuncios.html" class="boton-verde">Ver Todas</a>
+        <a href="anuncios.php" class="boton-verde">Ver Todas</a>
     </div>
 </section>
 
 <section class="imagen-contacto">
     <h2>Encuentra la casa de tus sueños</h2>
     <p>Llena el formulario de contacto y un asesor se pondrá en contacto contigo a la brevedad</p>
-    <a href="contacto.html" class="boton-amarillo">Contactános</a>
+    <a href="contacto.php" class="boton-amarillo">Contactános</a>
 </section>
 
 <div class="contenedor seccion seccion-inferior">
@@ -182,14 +147,14 @@
         <article class="entrada-blog">
             <div class="imagen">
                 <picture>
-                    <source srcset="build/img/blog1.webp" type="image/webp">
-                    <source srcset="build/img/blog1.jpg" type="image/jpeg">
-                    <img loading="lazy" src="build/img/blog1.jpg" alt="Texto Entrada Blog">
+                    <source srcset="./build/img/blog1.webp" type="image/webp">
+                    <source srcset="./build/img/blog1.jpg" type="image/jpeg">
+                    <img loading="lazy" src="./build/img/blog1.jpg" alt="Texto Entrada Blog">
                 </picture>
             </div>
 
             <div class="texto-entrada">
-                <a href="entrada.html">
+                <a href="entrada.php">
                     <h4>Terraza en el techo de tu casa</h4>
                     <p class="informacion-meta">Escrito el: <span>20/10/2021</span> por: <span>Admin</span> </p>
 
@@ -203,14 +168,14 @@
         <article class="entrada-blog">
             <div class="imagen">
                 <picture>
-                    <source srcset="build/img/blog2.webp" type="image/webp">
-                    <source srcset="build/img/blog2.jpg" type="image/jpeg">
-                    <img loading="lazy" src="build/img/blog2.jpg" alt="Texto Entrada Blog">
+                    <source srcset="./build/img/blog2.webp" type="image/webp">
+                    <source srcset="./build/img/blog2.jpg" type="image/jpeg">
+                    <img loading="lazy" src="./build/img/blog2.jpg" alt="Texto Entrada Blog">
                 </picture>
             </div>
 
             <div class="texto-entrada">
-                <a href="entrada.html">
+                <a href="entrada.php">
                     <h4>Guía para la decoración de tu hogar</h4>
                     <p class="informacion-meta">Escrito el: <span>20/10/2021</span> por: <span>Admin</span> </p>
 
@@ -233,20 +198,6 @@
         </div>
     </section>
 </div>
-
-<footer class="footer seccion">
-    <div class="contenedor contenedor-footer">
-        <nav class="navegacion">
-            <a href="./nosotros.html">Nosotros</a>
-            <a href="./anuncios.html">Anuncios</a>
-            <a href="./blog.html">Blog</a>
-            <a href="./contacto.html">Contacto</a>
-        </nav>
-    </div>
-
-    <p class="copyright">Todos los derechos Reservados 2021 &copy;</p>
-</footer>
-
-<script src="./build/js/bundle.min.js"></script>
-</body>
-</html>
+<?php
+include "./includes/templates/footer.php"
+?>
